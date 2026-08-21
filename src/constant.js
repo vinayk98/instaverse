ed2c80965ed74a80758ffcc45d9e631c0b955716
Vinay <vinay@Vinays-MacBook-Air.local>
2026-08-21 10:57:46 +0530

    minor changs

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
-export const BASE_URL =
-    import.meta.env.VITE_API_URL || 'http://localhost:8000';
+const API_URL = import.meta.env.VITE_API_URL;
+export const BASE_URL = API_URL;
 
 export const STATUS_CODES = {
     // Success
